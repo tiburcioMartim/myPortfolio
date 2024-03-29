c5461f42dfa8be86dd0a289937e01f8ca9d7087b
tiburcioMartim <tiburciomartim@gmail.com>
2024-03-29 20:53:22 -0300

    Atualizando profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/tiburcioMartim/myPortfolio/main/date/profile.json'
     const fetching = await fetch(url)
-    return await fetching.jason()    
+    return await fetching.json()    
 }
